daf8c4d99c739ea7dbeb0cf4e3fb985fed41da1a
moussiomar90 <79989418+moussiomar90@users.noreply.github.com>
2021-03-28 22:22:24 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('ama bo bta be')
+  response.send('TFK')
 }) 
 
 app.listen(app.get('port'), function() {
