2c663f0d0324a6219a5ba53cbd198c6da9d6323e
moussiomar90 <79989418+moussiomar90@users.noreply.github.com>
2021-03-28 21:48:03 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('mise a jours 21:43')
+  response.send('mise a jours 21:50')
 }) 
 
 app.listen(app.get('port'), function() {
