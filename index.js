495bd468ff60862e7816235aec72f9ce576ce3a4
moussiomar90 <79989418+moussiomar90@users.noreply.github.com>
2021-03-28 20:36:24 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello Testing DevOps Node! JS Project Hello How are you ? welcome to SAMA')
+  response.send('Bonjour c bon')
 }) 
 
 app.listen(app.get('port'), function() {
